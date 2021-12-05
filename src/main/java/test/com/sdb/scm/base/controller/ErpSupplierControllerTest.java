a24b496b382f9b47d6f2bba08b29461067c6c47c
hcttkx <1012703890@qq.com>
2021-12-05 18:21:58 +0800

    Initial commit

---
+++ src/main/java/test/com/sdb/scm/base/controller/ErpSupplierControllerTest.java
@@ -0,0 +1,85 @@
+package test.com.sdb.scm.base.controller;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* ErpSupplierController Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>12ÔÂ 5, 2021</pre> 
+* @version 1.0 
+*/ 
+public class ErpSupplierControllerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: list(ErpSupplier erpSupplier) 
+* 
+*/ 
+@Test
+public void testList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: export(ErpSupplier erpSupplier) 
+* 
+*/ 
+@Test
+public void testExport() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getInfo(@PathVariable("supplierId") Long supplierId) 
+* 
+*/ 
+@Test
+public void testGetInfo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: add(@RequestBody ErpSupplier erpSupplier) 
+* 
+*/ 
+@Test
+public void testAdd() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: edit(@RequestBody ErpSupplier erpSupplier) 
+* 
+*/ 
+@Test
+public void testEdit() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: remove(@PathVariable Long[] supplierIds) 
+* 
+*/ 
+@Test
+public void testRemove() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
